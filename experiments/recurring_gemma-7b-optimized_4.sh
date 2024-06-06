8efb8a4c34ab0692e66030e10c15923e0a269e75
Aditya Vikram Singh <aditya.11.present@gmail.com>
2024-06-06 08:14:01 +0000

    Added experiment scripts for gemma-7b

--- experiments/recurring_gemma-7b-optimized_4.sh
+++ experiments/recurring_gemma-7b-optimized_4.sh
@@ -3,16 +3,16 @@
 #SBATCH --mem=50G                   # Requested Memory
 #SBATCH -p gpu                      # Partition
 #SBATCH -G 1                        # Number of GPUs
-#SBATCH --constraint vram80         # GPU Memory
-#SBATCH -t 12:00:00                 # Job time limit
+#SBATCH --constraint vram48         # GPU Memory
+#SBATCH -t 2:00:00                  # Job time limit
 #SBATCH -o job-%j-logs.out          # %j = job ID
 
 module load miniconda/22.11.1-1
 conda activate cs685
 
-python3 src/reasoning/main.py --model "google/gemma-7b" --dataset "gsm8k" --dataset-size 8792 \
+python3 src/reasoning/main.py --model "google/gemma-7b" --dataset "gsm8k" --dataset-size 2198 --dataset-offset 6594 \
     --recurring-self-consistency \
     --num-shots 8 \
     --max-out-tokens 120 \
     --gpu-memory-utilization 0.9 \
-    --output-file "results/recurring__gsm8k__gemma-7b__output.json"
+    --output-file "results/recurring__gsm8k__gemma-7b__output-optimized_4.json"
